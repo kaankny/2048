12bace920e8a71904cc9336d9be6ebda50e12e46
Kaan Kanyılmaz <62248541+kaankny@users.noreply.github.com>
2024-06-14 21:07:07 +0300

    Add files via upload

---
+++ 2048/Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private TileBoard board;
+    [SerializeField] private CanvasGroup gameOver;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI hiscoreText;
+
+    public static string highScoreSaveKey = "highScore";
+
+    private int score;
+    public int Score => score;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        SetScore(0);
+        hiscoreText.text = LoadHighScore().ToString();
+        gameOver.alpha = 0f;
+        gameOver.interactable = false;
+        board.ClearBoard();
+        board.CreateTile();
+        board.CreateTile();
+        board.enabled = true;
+    }
+
+    public void GameOver()
+    {
+        board.enabled = false;
+        gameOver.interactable = true;
+        SaveHighScore();
+        StartCoroutine(Fade(gameOver, 1f, 1f));
+    }
+
+    private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay = 0f)
+    {
+        yield return new WaitForSeconds(delay);
+
+        float elapsed = 0f;
+        float duration = 0.5f;
+        float from = canvasGroup.alpha;
+
+        while (elapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = to;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        SetScore(score + points);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SaveHighScore()
+    {
+        int highScore = LoadHighScore();
+
+        if (score > highScore)
+            PlayerPrefs.SetInt(highScoreSaveKey, score);
+    }
+
+    private int LoadHighScore()
+    {
+        return PlayerPrefs.GetInt(highScoreSaveKey, 0);
+    }
+
+}
